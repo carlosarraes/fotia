dd6c581cce87aae9f95abc76e336d8e2e6ad9a78
carraes <carraeshb@gmail.com>
2023-07-18 10:59:02 -0300

    chore: setup theme provider/toast/next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import { api } from "@/utils/api";
-import "@/styles/globals.css";
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
+import { api } from '@/utils/api'
+import '@/styles/globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from 'react-hot-toast'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,9 +20,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <main className={roboto.className}>
+          <Toaster />
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
