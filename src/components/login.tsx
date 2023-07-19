17b86cbf74154a9f5ae00e55eb0c5c7b0ba6d153
carraes <carraeshb@gmail.com>
2023-07-19 00:14:07 -0300

    feat: create login component

---
+++ src/components/login.tsx
@@ -0,0 +1,23 @@
+import { signIn } from 'next-auth/react'
+import { Button } from './ui/button'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
+import { FaGoogle } from 'react-icons/fa'
+
+const Login = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>Entrar</DialogTrigger>
+      <DialogContent className="bg-gray-50 dark:bg-slate-800">
+        <DialogHeader>
+          <DialogTitle>Faça login com sua conta:</DialogTitle>
+        </DialogHeader>
+        <Button type="button" variant="outline" onClick={() => void signIn('google')}>
+          <FaGoogle />
+          <span className="ml-2">com Google</span>
+        </Button>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default Login
