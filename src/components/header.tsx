74ce78155bffcbb8f958da1474f4f1a400258743
carraes <carraeshb@gmail.com>
2023-07-19 00:13:50 -0300

    style: some class changes to base componets

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,8 @@
 import { Button } from './ui/button'
-import { FaGoogle } from 'react-icons/fa'
+import { FaCameraRetro } from 'react-icons/fa'
 import { ModeToggle } from './ui/mode-toggle'
-import { Separator } from './ui/separator'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
+import Login from './login'
 
 const Header = () => {
   const { status } = useSession()
@@ -11,30 +10,21 @@ const Header = () => {
   console.log(status)
 
   return (
-    <header className="flex w-full justify-between bg-transparent px-10 py-4">
-      <h1 className="font-bold tracking-wider text-xl">
-        Fot<span className="text-sky-700">IA</span>
-      </h1>
+    <header className="flex w-full justify-between bg-transparent px-10 py-4 border-b border-x border-slate-800">
+      <div className="flex items-center justify-center gap-2 text-xl">
+        <FaCameraRetro className="inline-block text-sky-600" />
+        <div className="font-bold tracking-wider">
+          Fot<span className="text-sky-600">IA</span>
+        </div>
+      </div>
       <div className="flex space-x-6">
         <ModeToggle />
-        <Separator orientation="vertical" />
         {status === 'authenticated' ? (
           <Button type="button" onClick={() => void signOut()}>
             <span className="text-lg">Sair</span>
           </Button>
         ) : (
-          <Dialog>
-            <DialogTrigger>Entrar</DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Faça login com sua conta:</DialogTitle>
-              </DialogHeader>
-              <Button type="button" variant="outline" onClick={() => void signIn('google')}>
-                <FaGoogle />
-                <span className="ml-2">com Google</span>
-              </Button>
-            </DialogContent>
-          </Dialog>
+          <Login />
         )}
       </div>
     </header>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import Header from './header'
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
-    <section className="mx-auto flex h-full w-full max-w-4xl flex-col items-center justify-center border-2 border-red-500">
+    <section className="mx-auto flex h-screen w-full max-w-4xl flex-col items-center justify-center">
       <Header />
       {children}
     </section>
