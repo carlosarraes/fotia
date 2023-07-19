193e0838fcc2ce8086d29c210c8f99e6acf913f1
carraes <carraeshb@gmail.com>
2023-07-19 12:18:10 -0300

    feat: init dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/layout'
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <section className="flex flex-col w-full justify-center items-start text-center md:text-left p-4 border-x border-b h-screen border-slate-800">
+        <h1>Dashboard</h1>
+      </section>
+    </Layout>
+  )
+}
+
+export default Dashboard
