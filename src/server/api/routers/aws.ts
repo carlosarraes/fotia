aa200091ec0f47f7f6cadc5db3b8bf297dee75a4
carraes <carraeshb@gmail.com>
2023-07-21 17:20:28 -0300

    refactor: extract gellAlluserImgs, make it fitler zip files

--- src/server/api/routers/aws.ts
+++ src/server/api/routers/aws.ts
@@ -2,83 +2,13 @@ import { createTRPCRouter, protectedProcedure } from '@/server/api/trpc'
 import { env } from '@/env.mjs'
 import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
-import {
-  PutObjectCommand,
-  ListObjectsV2Command,
-  GetObjectCommand,
-  DeleteObjectCommand,
-} from '@aws-sdk/client-s3'
+import { PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
 import type { PutObjectCommandInput } from '@aws-sdk/client-s3'
 import { s3 } from '@/utils/s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { getAllUserImgs } from '@/utils/getAllImgs'
 
-export const getAllUserImgs = async (userId: string) => {
-  const path = `fotia/${userId}/`
-
-  const { Contents } = await s3.send(
-    new ListObjectsV2Command({
-      Bucket: env.AWS_BUCKET_NAME,
-      Prefix: path,
-    }),
-  )
-
-  if (!Contents) {
-    throw new TRPCError({
-      code: 'NOT_FOUND',
-      message: 'No images found',
-    })
-  }
-
-  const objCommands = Contents.map(({ Key }) => {
-    return new GetObjectCommand({
-      Bucket: env.AWS_BUCKET_NAME,
-      Key: Key as string,
-    })
-  })
-
-  const signedUrls = await Promise.all(
-    objCommands.map((command) =>
-      getSignedUrl(s3, command, {
-        expiresIn: 120,
-      }),
-    ),
-  )
-
-  const signedUrlsWithKeys = signedUrls.map((url, i) => {
-    if (!Contents[i]) {
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: 'No images found',
-      })
-    }
-
-    const key = Contents[i]?.Key
-    if (!key) {
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: 'No images found',
-      })
-    }
-
-    const id = key.split('/')[2]
-    if (!id) {
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: 'No images found',
-      })
-    }
-
-    return {
-      url,
-      key,
-      id,
-    }
-  })
-
-  return signedUrlsWithKeys
-}
-
-const awsRouter = createTRPCRouter({
+export const awsRouter = createTRPCRouter({
   uploadImgs: protectedProcedure
     .input(
       z.object({
@@ -139,5 +69,3 @@ const awsRouter = createTRPCRouter({
       }
     }),
 })
-
-export default awsRouter
---
+++ src/utils/getAllImgs.ts
@@ -0,0 +1,71 @@
+import { GetObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3'
+import { s3 } from './s3'
+import { env } from '@/env.mjs'
+import { TRPCError } from '@trpc/server'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+
+export const getAllUserImgs = async (userId: string) => {
+  const path = `fotia/${userId}/`
+
+  const { Contents } = await s3.send(
+    new ListObjectsV2Command({
+      Bucket: env.AWS_BUCKET_NAME,
+      Prefix: path,
+    }),
+  )
+
+  if (!Contents) {
+    throw new TRPCError({
+      code: 'NOT_FOUND',
+      message: 'No images found',
+    })
+  }
+
+  const objCommands = Contents.map(({ Key }) => {
+    return new GetObjectCommand({
+      Bucket: env.AWS_BUCKET_NAME,
+      Key: Key as string,
+    })
+  })
+
+  const signedUrls = await Promise.all(
+    objCommands.map((command) =>
+      getSignedUrl(s3, command, {
+        expiresIn: 120,
+      }),
+    ),
+  )
+
+  const mappedUrls = signedUrls.map((url, i) => {
+    if (!Contents[i]) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'No images found',
+      })
+    }
+
+    const key = Contents[i]?.Key
+    if (!key) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'No images found',
+      })
+    }
+
+    const id = key.split('/')[2]
+    if (!id) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'No images found',
+      })
+    }
+
+    return {
+      url,
+      key,
+      id,
+    }
+  })
+
+  return mappedUrls.filter(({ id }) => id !== 'data.zip')
+}
